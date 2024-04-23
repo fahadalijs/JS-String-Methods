8dcf85c7af8db1f0e1ee867bed2231158c0fabc8
fahadalijs <fahad9827022@gmail.com>
2024-04-23 16:15:15 +0500

    JS-String-Method

---
+++ app.js
@@ -0,0 +1,119 @@
+// console.log(String-Method);
+
+// toLowerCase
+// toUpperCase
+// length
+// charAt
+// indexOf
+// slice
+// replace
+// replaceall
+// split
+
+
+
+// toLowerCase (string ko lower case ma convert kr deta ha)
+
+// let Myname = "FAHAD ALI"
+// console.log(Myname.toLowerCase());
+
+
+// toUpperCase (String ko upper case ma convert kr da ga);
+// let Myname = "Fahad Ali"
+// console.log(Myname.toUpperCase());
+
+
+
+// length(string ki length console ma print krwa da ga);
+
+// let Myname = "Fahad"
+// console.log(Myname.length);
+
+
+
+
+
+// Slice (String ma ap ko jo chiya ap ko wo dekha da ga) 
+
+// let Myname = "Fahad"
+// console.log(Myname.slice(2 , 4));
+
+
+
+
+
+
+// replace (String ma ak particular cheese ko dosri cheese sa replace kr deta ha)
+
+// const Myname = "Fahad Ali"
+// console.log(Myname.replace("Fahad Ali" , "Mukhtar"));
+
+
+
+
+
+// let message = "Ali and Sami are best and friends. They play cricket Ali and Sami and football Ali and Sami together.";
+
+// console.log(message.replaceAll("Ali" , "Abdulallah"));
+
+
+
+
+
+// const city = prompt("Which city do yu live?");
+
+// if(city.toLowerCase()=== "karachi"){
+//     console.log("You live in safest city");
+// }else{
+//     console.log("Zindagi jant ha");
+// }
+
+
+
+
+// const city = prompt("Which city do you live")
+// if(city.toLocaleLowerCase() === "karachi"){
+//     console.log("You live in safest city");
+// }else{
+//     console.log("Zindagi jant ha");
+// }
+
+
+
+// let cities = ['karachi' , 'hyderabad' , 'larkana' , 'sukkar' , 'jamshoro' , 'shikarpur' , 'lahore' , 'gujrawala' , 'sahiwal' , 'peshawar' , 'quetta'];
+
+// const city = document.querySelector("#city")
+
+// function  cheakcities(){
+//     if(cities.includes(cities.value)){
+//         console.log("You live in the safest city");
+//     }else{
+//         console.log("Malik muskil ha");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
